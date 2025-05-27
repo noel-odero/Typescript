0f13613d9046eb5b62eddaa5db8500959495dbe5
Noel Odero <anoel.odero@gmail.com>
2025-05-27 07:11:12 +0200

    interfaces

--- ts-setup/dist/interfaces-learn-types.js
+++ ts-setup/dist/interfaces-learn-types.js
@@ -10,3 +10,6 @@ class GoldenChase {
         this.address = address;
     }
 }
+const GCBangalore = new GoldenChase('Golden Chase Arcae', 'string');
+// the main difference between interfaces and type aliases is that interfaces can be extended and implemented, while type aliases cannot.
+// interfaces can be used to define the shape of an object, while type aliases can be used to define a type that can be a primitive, union, or intersection type.
